ce9b3ae7e2f748eb80fa9502cd1de9b2514ec152
catherineranran <catherineranran@gmail.com>
2026-08-26 00:49:17 +0200

    Update ecology external circle years

--- script/ecology.js
+++ script/ecology.js
@@ -87,8 +87,8 @@ const ecologyNodes = [
     { id: "william-fleeson", name: "William Fleeson", knownSince: 2026, field: "", expertise: "", affiliation: "" },
     { id: "shaul-oreg", name: "Shaul Oreg", knownSince: 2026, field: "", expertise: "", affiliation: "" },
     { id: "anna-baumert", name: "Anna Baumert", knownSince: 2026, field: "", expertise: "", affiliation: "" },
-    { id: "reeshad-s-dalal", name: "Reeshad S. Dalal", knownSince: 2026, field: "", expertise: "", affiliation: "" },
-    { id: "simon-columbus", name: "Simon Columbus", knownSince: 2026, field: "", expertise: "", affiliation: "" },
+    { id: "reeshad-s-dalal", name: "Reeshad S. Dalal", knownSince: 2025, field: "", expertise: "", affiliation: "" },
+    { id: "simon-columbus", name: "Simon Columbus", knownSince: 2022, field: "", expertise: "", affiliation: "" },
 ];
 
 const ecologyEdges = [
