044a4307b55259b99b4e7f6521b92368a3264e95
catherineranran <catherineranran@gmail.com>
2026-09-18 15:37:33 +0200

    Update bystandership workspace password

--- script/trait-state-bystandership.js
+++ script/trait-state-bystandership.js
@@ -3,12 +3,12 @@ window.projectWorkspaceConfig = {
     encryptedHtml: {
         title: "Trait–State Guardianship: Analysis and Results",
         ciphertext: window.traitStateBystandershipReport,
-        salt: "BEwiKpvUK+VUyULo0zeaGQ==",
-        iv: "7JRnHOfZDMH9Mr8h",
+        salt: "px3WiRMp14MzZlYTyiWuug==",
+        iv: "7jXFLiwWHgitqHQa",
     },
     encryptedProject: {
-        salt: "yCJcp7hxAaHrohKGzkMtjA==",
-        iv: "H+RlaYxz2wcNL8ZD",
-        ciphertext: "21Oty90QYDW40dGDZrE+cqhf54itYD+5JXSNG+FGyhfvVI3mY5rcLnIA8WVWL+5u9SbfWcAu8fyvA4AFCS1zMlybbpdfl8CT/Efu0sOr7xDrVcY3yKoRaUQmktH0eKMidv4nxHXiCmWS2hMjlyaGgpYwXAQ9BB5vBLHjyUmGhkgoeEIk8Hs+zjDDH7aMTU7CP1zQ3BV+Fih1rHG/Te49U6IbNV9YSCG5BTAZ9xM6ppkVGVxf5LNNLsjs/F9I40PJ348CSis3nUyFPe8CcDs8t09gFLYqe1kI/WV/jKuHilbwZ1E1eNWU0mQMAuHg11p0h19HX3enCzOXNu3egaV4PwDwhXlrYbcNcSQzNZ4uzAUYtVxeSqJK2gffPozmHZ7/3mmhkmt0/it2qAwFgZ15007RbIYRceURbvm2Th8ORxuHSFzaaiKFU/J1a4rVHG/jRdNFtz+WezoFmLfRJEpXNL1Hv6qE9z743TaHCdCNR6VSa20EErRRgjEaKon9OYT+pNuUQWn72oYzGue5famVY/TZu9lpLWYb88porL/+pU0M2PyocsXqUMpZ2N772MjKdC5fmoJ+aaberIw1bmO3Gh42LTMwmTcqDXyTGBRcIKrNYdyzM7TNQYC8doKetVaPxDN41AHGP5LtNrATNktCoSLhBhsO438IjLWLrwrco0CeeTZAnR3gOaCP9zRmpVyezLw9ARIkjJ+k2Eo6x/MkfCVotKXI57fikIAQk6OtT03duVjEcs+bJq/r/mJlyl9KmwymZSUKMU4Wj/tpWmFUCnxEsLWBW2uqptK7P5qjPsygCpLXGa3IJpvmaqxXOOG+77/AW/pHueWicd+1pXBbOAh5C/iD+v0Wd4C1tebloBqdTTtLQRO0CpYVxkMXFHyoUYByGdo=",
+        salt: "O44rm/VF93peeWUld3MxKA==",
+        iv: "QYHfjT+9R3OX+LnN",
+        ciphertext: "r4hJfcPlE7bmLxJ5KQjm9BYOyOiDKgv3iLoZMwGQ6p2Odf+1yr6z/Ly0JgjkK26GLTBZulHC00jDqKprC/FcfNKnyedkY605pPHC4wM2ZjxRDiCQ8h6IBPEjH/WfGdacaHZ+cYeWk5/5qkGxboHtBV222lhjZEuWsWxLq9Y3Juqe5wY2pOWpyTwNWqrsSkTB4qQ2k2F+fW3cuu4JY260E2EvUVv6Tf5FKqEG/nA6YNaeVbJ+me/dslmkpjXeIrFHigaPD55qnItsPY+Bblhu29c8KrN27TqTbIq//UdAPZoxEsyu7CTqddyu1jUvDHadiQMoj/nMeQO7m2NQHzwyVglA+NT4aCPbKxXL3+jYrnt5CRHEk44dSqkJ1dAVe1swmezofjJcbN/eTglpvMjpMhz6NePU0+8s4YuK35gGTDPC8EknVLon4sHTSGXgQdOKmY55UfhBBHU6YoJKmbwMQPosDQt0zGH166+r6fKYD0/sRMlw8rqjlDTSpJ9tkBbhi3qxFMVKKYcBOnaO45hD5zn9LqdIkk4z+vD8tcLcrkQzP2Yx96Tg+sOJf1f+TQgzIF6PfZ/wSuqPigq0wk4eYrFWXMPloAK4XyekKcCSBuH817gXBE9DiA2EyUSrzNamF9Tzef3LZXZsbQxUnqOEeawdFmxb8ey2zjhx8OHmdhitRA+9gsP/1r6lmw8KVg7EG22sHxv7y0KJd5lbFOEFkJ7bxvENQxUhxaKeVANJZzKsjirjtLC3slGk/ygYmpcagC5fCX41y10fdmRotjcUJ1veU0JUWGAT4ANW8bxfR9K1bIUHsPbxLDIBOKNB0+dxBtx1MVsf5Q461i2qG1dpIfbdM5bN3KvpLuZlpZ0LfGNV9QfVQYolsWia0caUzgeiERY1SpQ=",
     },
 };
