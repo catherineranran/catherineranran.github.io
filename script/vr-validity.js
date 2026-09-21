7567249919cda5070635023d3a06c69aa8720ed8
catherineranran <catherineranran@gmail.com>
2026-09-21 22:54:18 +0200

    Add VR validity workspace

---
+++ script/vr-validity.js
@@ -0,0 +1,8 @@
+window.projectWorkspaceConfig = {
+    eyebrow: "Validity beyond presence",
+    encryptedProject: {
+        salt: "omccC3KezOiqa9sIijKwSw==",
+        iv: "qvErdgxuXHjaRIgB",
+        ciphertext: "FKFHjo2dsZlC5GEOQMzZsK4O1FJyfa8+WbrU1fqrCAKSsLZ1Riqkr8OsCC/c4uGtDExjR1cAQXqNVaXS7WKkP5u4XdhI3jpIp2CvsL7lIL2S+6Ji0RM1Vgh8Aae0KjRFIpLjhhuzqYdrr8SI4R2Q0lunuXBruEJCJVlz5lMgywRPCZ4Wq/HFgDpkSt4z1gUbEKRCRIS8EEsWjuTKn0o5dDZOEUxEOolExseF+NUI/x1VljuohM7kMwGHlybdWG3gZhJfBNTtPITGkPlpV4ElPkSqlwrDTOL+bG5NC9FjKIXaMdmsW6w97BtiYdpSoeCJ4NBPXWe9R/+BQ/kG0FLJwlQHpG8A9+3mQQwyPVTOTAjDfLWvMehkDYGjhbY/eh3lq8UgMuhurCcHwj9MXTUocbDGv1hXzAHtvmOGzhkg5AJaQWDCZvsfFs7frxkLZYmn7X+LbBRFf6XlP90BEC4gxDSkTV2RA1sRlWhK4D4sXafE8mnkhw7UyElpzi9BPNThvf2GXjPAITvrCEAkt4BAg5dr+1OQ8jxYh1UzowKBCCXK01Cc1+eJ5URAtpfNwaRmXVJHGvyy2qXj4Na6TIbcgVijbOMUkQZx4nA7fQw9L0PlFtUfBiFrxI47rfXB4q3XrD3VvUxxS3xa0RIqSSmLzN/yEKdaWzjCt9NkZXZLqVmCDaPqL5jQIufZDRfOwXnJeMK9/IJdaWJglusqUkCxNyfdcbIBMHlvyKTHJWGx0OspM/Evl1WvztXMCrvOoYFIew6JXFX5HsuSXA==",
+    },
+};
